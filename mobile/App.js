b7a7e0845f1a6c176a25e690ae61eeaf2235cff6
punisher077 <arthur.mts@gmail.com>
2020-01-16 11:58:19 -0300

    criando rotas do app

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import Routes from './src/routes';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.bold} >Hello world</Text>
     </View>
   );
 }
@@ -12,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bold: {
+    fontWeight: "bold",
+    color: "#fff",
+    fontSize: 40
+  }
 });
+
